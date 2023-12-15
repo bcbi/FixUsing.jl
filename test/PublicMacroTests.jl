625286bcfd04dc187a7906929d71a150133e4f55
Dilum Aluthge <dilum@aluthge.com>
2023-12-15 00:33:18 -0500

    Add more tests

--- test/PublicMacroTests.jl
+++ test/PublicMacroTests.jl
@@ -6,11 +6,17 @@ using FixUsing.PublicMacro: @public
 
 @public f
 @public @hello
+@public (g, h, @world)
 
 function f end
+function g end
+function h end
 
 macro hello()
 end
+
+macro world()
+  end
 
 end
 
